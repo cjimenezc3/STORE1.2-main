2f487af622f894ea79b60628e2ce4fcef60f9dcc
cjimenezc3 <73454683+cjimenezc3@users.noreply.github.com>
2021-10-22 22:30:28 -0600

    Update and rename library/configServer.php to biblioteca/configServer.php

---
+++ biblioteca/configServer.php
@@ -0,0 +1,13 @@
+<?php
+define("USER", "storeadmin");
+define("SERVER", "storegrupo33.database.windows.net");
+define("BD", "store");
+define("PASS", "Thebest2527");
+
+
+
+/*<?php
+define("USER", "root");
+define("SERVER", "localhost");
+define("BD", "store");
+define("PASS", "Thebest2527");
